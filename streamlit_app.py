e8bc8157494a7068c19311db974f1386dccf524a
Vinay18101998 <96097037+Vinay18101998@users.noreply.github.com>
2022-11-03 00:57:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,18 @@
 import streamlit
 streamlit.title('My Parents new healthy dines')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 and blueberry Oatmeal')
+streamlit.text('kale,Spinach and Rocket smoothie')
+streamlit.text('Hard_Boiled Free_Range Egg')
+
+
+
+
+
+
+
+
+
+
+
+
