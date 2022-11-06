0a40e7c4b585553ba99c139d7b30ebdc67659f07
Vinay18101998 <96097037+Vinay18101998@users.noreply.github.com>
2022-11-06 08:46:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,3 +52,4 @@ streamlit.write('Thanks for adding ', fruit_choice)
 
 my_cur.execute("insert into fruit_load_list values('from streamlit')")
 
+my_cur.execute("insert into fruit_load_list values('from streamlit')")
